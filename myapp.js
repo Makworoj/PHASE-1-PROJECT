b5d74c50dfcc1cb5812e2b6e39773daaade5dc03
Makworoj <makworomokaya@gmail.com>
2025-10-15 19:22:04 +0300

    started updating JS file

---
+++ myapp.js
@@ -0,0 +1,6 @@
+const facilityList = document.getElementById("facility-list");
+const searchInput = document.getElementById("search");
+const countyFilter = document.getElementById("county-filter");
+const typeFilter = document.getElementById("type-filter");
+const nhifFilter = document.getElementById("nhif-filter");
+const themeToggle = document.getElementById("theme-toggle");§§§
